0e7e6f28581e1785a26012498fee1a3281dcaadd
MaSanto Raffiki <Davidpaez1006@gmail.com>
2019-05-15 23:20:30 -0600

    Update cam_email.py

--- cam_email.py
+++ cam_email.py
@@ -1,69 +1,52 @@
-import picamera
-import time
-import smtplib, ssl
-from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
-from email.mime.text import MIMEText
-from email.utils import formatdate
-from email import encoders
+import smtplib,ssl  
+from picamera import PiCamera  
+from time import sleep  
+from email.mime.multipart import MIMEMultipart  
+from email.mime.base import MIMEBase  
+from email.mime.text import MIMEText  
+from email.utils import formatdate  
+from email import encoders  
+  
+camera = PiCamera()  
 
-sender = 'davidpaez1006@gmail.com'
-password = '**********!'
-receiver = 'davidpaez1006@gmail.com'
-
-DIR = '/home/security/Pictures/'
-FILE_PREFIX = 'homesnap.jpg'
-
-            
-
-
-def cheese_MOFO():
-    print("Photo and Email Init....")
-    # Create the directory if not exists
-    if not os.path.exists(DIR):
-        os.makedirs(DIR)
-    # Find the largest ID of existing images.
-    # Start new images after this ID value.
-    files = sorted(glob.glob(os.path.join(DIR, FILE_PREFIX + '[0-9][0-9][0-9].jpg')))
-    count = 0
+def cheeze_MOFO():
+    camera.rotation = 180    
+    camera.start_preview()  
+    sleep(5)  
+    camera.capture('/home/pi/image.jpg')     # image path set
+    sleep(5)  
+    camera.stop_preview()
     
-    if len(files) > 0:
-        # Grab the count from the last filename.
-        count = int(files[-1][-7:-4])+1
-
-    # Save image to file
-    filename = os.path.join(DIR, FILE_PREFIX + '%03d.jpg' % count)
-    # Capture the face
-    with picamera.PiCamera() as camera:
-        pic = camera.capture(filename)
-    # Sending mail
-    msg = MIMEMultipart()
-    msg['From'] = sender
-    msg['To'] = receiver
-    msg['Subject'] = 'Movement Detected'
-    
-    body = 'Picture of Security System.'
-    msg.attach(MIMEText(body, 'plain'))
-    attachment = open(filename, 'rb')
-    part = MIMEBase('application', 'octet-stream')
-    part.set_payload((attachment).read())
-    encoders.encode_base64(part)
-    part.add_header('Content-Disposition', 'attachment; filename= %s' % filename)
-    msg.attach(part)
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(sender, password)
-    text = msg.as_string()
-    server.sendmail(sender, receiver, text)
-    print("Email Sent.....")
-    server.quit()
-
-cheese_MOFO()
-
-
-#   camera.start_preview()
-#  camera.start_recording('/home/pi/video.h264')
-# time.sleep(10)
-#camera.stop_recording()
-# camera.stop_preview()
-
+def send_an_email():  
+    toaddr = 'email@gmail.com'      # To id 
+    me = 'email@gmail.com'          # your id
+    subject = "Picture frm Home!"              # Subject
+  
+    msg = MIMEMultipart()  
+    msg['Subject'] = subject  
+    msg['From'] = me  
+    msg['To'] = toaddr  
+    msg.preamble = "test "   
+    #msg.attach(MIMEText(text))  
+  
+    part = MIMEBase('application', "octet-stream")  
+    part.set_payload(open("/home/pi/image.jpg", "rb").read())  
+    encoders.encode_base64(part)  
+    part.add_header('Content-Disposition', 'attachment; filename="image.jpg"')   # File name and format name
+    msg.attach(part)  
+  
+    try:  
+       s = smtplib.SMTP('smtp.gmail.com', 587)  # Protocol
+       s.ehlo()  
+       s.starttls()  
+       s.ehlo()  
+       s.login(user = me, password = '**********')  # User id & password
+       #s.send_message(msg)  
+       s.sendmail(me, toaddr, msg.as_string())  
+       s.quit()  
+    #except:  
+    #   print ("Error: unable to send email")    
+    except SMTPException as error:  
+          print ("Error")                # Exception
+cheeze_MOFO()  
+send_an_email()  
